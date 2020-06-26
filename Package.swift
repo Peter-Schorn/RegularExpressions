dcc2807e77ebfd9704db3475d0607b0d94dd301b
Peter-Schorn <58197311+Peter-Schorn@users.noreply.github.com>
2020-06-26 03:17:54 -0500

    Declared supported platforms and supported swift version—Swift 5.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "RegularExpressions",
+    platforms: [
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries
         // produced by a package, and make them visible to other packages.
@@ -31,5 +37,7 @@ let package = Package(
             dependencies: ["RegularExpressions"]
         )
         
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
+    
 )
